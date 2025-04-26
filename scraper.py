7c80977af5e93017913b02cc904a7b2c6c6ca452
zak1995 <111587934+zak1995@users.noreply.github.com>
2025-04-26 17:18:09 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,11 @@
+from playwright.sync_api import sync_playwright
+
+def universal_scrape(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url, timeout=60000)
+        content = page.content()
+        browser.close()
+        return content
 
